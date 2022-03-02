45c2a4c330c835901709a4025762b0bc51e60b66
amancu <amancu@neuro.mpg.de>
2022-03-02 15:15:07 +0100

    Dataset segementation conversion to neuroglancer formats

--- scripts/dataset_specific/segmentation_rag_flat_v3_to_precomputed.py
+++ scripts/dataset_specific/segmentation_rag_flat_v3_to_precomputed.py
@@ -8,9 +8,9 @@ from cloudvolume.lib import mkdir, touch
 from concurrent.futures import ProcessPoolExecutor
 import time
 
-kd = KnossosDataset("/ssdscratch/songbird/j0251/segmentation/j0251_72_seg_20210127_agglo2/knossos.pyk.conf")
+kd = KnossosDataset("/ssdscratch/songbird/j0251/segmentation/j0251_rag_flat_Jan2019_seg/knossos.pyk.conf")
 
-volume_info8 = volume_info8 = {
+volume_info8 = {
         "type": "segmentation",
         "layer_type": "segmentation",
         "data_type": "uint64",
@@ -20,7 +20,7 @@ volume_info8 = volume_info8 = {
                 "key": "8_8_8",
                 "size": [3389, 3418, 1936],
                 "resolution": [80, 80, 200],
-                "chunk_sizes": [[64, 64, 64]],
+                "chunk_sizes": [[128, 128, 128]],
                 "encoding": "compressed_segmentation",
                 "compressed_segmentation_block_size": [8,8,8],
                 "factor": (8,8,8),
@@ -28,7 +28,7 @@ volume_info8 = volume_info8 = {
         ]
     }
 
-vol8 = CloudVolume('file:///wholebrain/scratch/amancu/j0251/agglo2/agglo2_precomputed_mag8/', info=volume_info8, bounded=False, non_aligned_writes=True)
+vol8 = CloudVolume('file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag8/', info=volume_info8, bounded=False, non_aligned_writes=True)
 vol8.provenance.description = 'Whole j0251 segmentation conversion mag 8'
 vol8.provenance.owners = ['kornfeld@neuro.mpg.de','amancu@neuro.mpg.de', 'hashir@neuro.mpg.de']  # list of contact email addresses
 vol8.commit_info()  # generates file://.../info json file
@@ -36,7 +36,7 @@ vol8.commit_provenance()  # generates file://.../provenance json file
 
 to_upload = []
 # split dataset
-num_x, num_y, num_z = kd.boundary[0]//512, kd.boundary[1]//512, kd.boundary[2]//512
+num_x, num_y, num_z = kd.boundary[0]//1024, kd.boundary[1]//1024, kd.boundary[2]//1024
 print(f"num_x: {num_x}")
 print(f'Splitting dataset into number of chunks on each xyz-axis: {num_x}, {num_y}, {num_z}')
 for x in range(num_x+1):          # +1 for the boundary chunks
@@ -49,25 +49,26 @@ def process(z):
     # global kd
     try:
         ####### MAG 8 ####################################################################################################################
-        x_start = z[0] * 512
-        y_start = z[1] * 512
-        z_start = z[2] * 512
+        x_start = z[0] * 1024
+        y_start = z[1] * 1024
+        z_start = z[2] * 1024
         # check for boundary chunk ends for knossos
-        x_end = (x_start + 512) if (x_start + 512) < kd.boundary[0] else kd.boundary[0]
-        y_end = (y_start + 512) if (y_start + 512) < kd.boundary[1] else kd.boundary[1]
-        z_end = (z_start + 512) if (z_start + 512) < kd.boundary[2] else kd.boundary[2]
+        x_end = (x_start + 1024) if (x_start + 1024) < kd.boundary[0] else kd.boundary[0]
+        y_end = (y_start + 1024) if (y_start + 1024) < kd.boundary[1] else kd.boundary[1]
+        z_end = (z_start + 1024) if (z_start + 1024) < kd.boundary[2] else kd.boundary[2]
         print(f"Computing chunk {x_start}:{x_end}, {y_start}:{y_end} {z_start}:{z_end}")
         size = (x_end-x_start, y_end-y_start, z_end-z_start)
         # get bytes array from Knossos and reshape it to the according required size
         bytes_array = kd.load_seg(offset=(x_start, y_start, z_start), size=size, mag=8)     # Knossos returns zyx arrays
         bytes_array = np.swapaxes(bytes_array,0,2)                                          # puts it in xyz
         # check for boundary chunk ends for volume
-        x_start = z[0] * 64
-        y_start = z[1] * 64
-        z_start = z[2] * 64
-        x_end = x_start + 64 if (x_start + 64) < 3389 else 3389
-        y_end = y_start + 64 if (y_start + 64) < 3418 else 3418
-        z_end = z_start + 64 if (z_start + 64) < 1936 else 1936
+        x_start = z[0] * 128
+        y_start = z[1] * 128
+        z_start = z[2] * 128
+
+        x_end = x_start + 128 if (x_start + 128) < 3389 else 3389
+        y_end = y_start + 128 if (y_start + 128) < 3418 else 3418
+        z_end = z_start + 128 if (z_start + 128) < 1936 else 1936
         vol8[x_start:x_end, y_start:y_end, z_start:z_end] = bytes_array
 
     except IOError as err:
@@ -78,5 +79,7 @@ def process(z):
       print ('Unexpected error:', sys.exc_info())
       raise
 
-with ProcessPoolExecutor(max_workers=10) as executor:
+with ProcessPoolExecutor(max_workers=18) as executor:
     executor.map(process, to_upload)
+
+vol8[0:1000,0:1000,0:500].viewer(port=8000)
---
+++ scripts/dataset_specific/segmentation_rag_flat_v3_to_sharded.py
@@ -0,0 +1,126 @@
+from taskqueue import LocalTaskQueue
+import igneous.task_creation as tc
+
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag1/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag1/'
+
+# print(f'Start mag 1')
+
+# with LocalTaskQueue(parallel=20) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(64,64,64),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag2/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag2/'
+
+# print(f'Start mag 2')
+
+# with LocalTaskQueue(parallel=20) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(64,64,64),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag4/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag4/'
+
+# print(f'Start mag 4')
+
+# with LocalTaskQueue(parallel=20) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(64,64,64),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+src_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag8/'
+dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag8/'
+
+print(f'Start mag 8')
+
+with LocalTaskQueue(parallel=20) as tq:
+
+    tasks = tc.create_image_shard_transfer_tasks(
+        src_path, dest_path,
+        mip=0, chunk_size=(64,64,64),
+        encoding=None, bounds=None, fill_missing=True,
+        translate=(0, 0, 0), dest_voxel_offset=None,
+        agglomerate=False, timestamp=None,
+        memory_target=3.5e9,
+    )
+    tq.insert_all(tasks)
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag16/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag16/'
+
+# print(f'Start mag 16')
+
+# with LocalTaskQueue(parallel=20) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(32,32,32),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag32/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag32/'
+
+# print(f'Start mag 32')
+
+# with LocalTaskQueue(parallel=1) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(16,16,16),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+
+# src_path = 'file:///wholebrain/scratch/anaml/j0251/rag_flat_v3/rag_flat_v3_precomputed_mag64/compressed_segmentation/'
+# dest_path = 'file:///wholebrain/scratch/amancu/j0251/rag_flat_v3/rag_flat_v3_sharded_mag64/'
+
+# print(f'Start mag 64')
+
+# with LocalTaskQueue(parallel=20) as tq:
+
+#     tasks = tc.create_image_shard_transfer_tasks(
+#         src_path, dest_path,
+#         mip=0, chunk_size=(8,8,8),
+#         encoding=None, bounds=None, fill_missing=True,
+#         translate=(0, 0, 0), dest_voxel_offset=None,
+#         agglomerate=False, timestamp=None,
+#         memory_target=3.5e9,
+#     )
+#     tq.insert_all(tasks)
+
+print("Done!")
