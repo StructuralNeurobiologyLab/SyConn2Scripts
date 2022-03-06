20628e0e07f756b3b614473c727295b35212e5eb
amancu <amancu@neuro.mpg.de>
2022-03-06 14:11:10 +0100

    update for ultrastructures

--- scripts/dataset_specific/rag_flat_v3_meshes_to_cloudvolume.py
+++ scripts/dataset_specific/rag_flat_v3_meshes_to_cloudvolume.py
@@ -13,6 +13,7 @@ from concurrent.futures import ProcessPoolExecutor
 from syconn import global_params
 from syconn.reps.segmentation import SegmentationDataset
 from syconn.reps.super_segmentation import SuperSegmentationObject, SuperSegmentationDataset
+import syconn.reps.segmentation_helper as helper
 
 from cloudvolume.datasource.precomputed.metadata import PrecomputedMetadata
 from cloudvolume.cacheservice import CacheService
@@ -44,7 +45,7 @@ volume_info = {
         ]
     }
 
-vol_sv = create_precomputed(cloudpath + "sv/", fill_missing=True, info=volume_info, mesh_dir='mesh')
+vol_sv = create_precomputed(cloudpath, fill_missing=True, info=volume_info, mesh_dir='mesh')
 vol_sv.provenance.description = 'Whole j0251 dataset meshes'
 vol_sv.provenance.owners = ['kornfeld@neuro.mpg.de','amancu@neuro.mpg.de', 'hashir@neuro.mpg.de', 'lacatusu@neuro.mpg.de']
 vol_sv.commit_info()  # generates file://.../info json file
@@ -69,14 +70,14 @@ vol_sj.commit_info()  # generates file://.../info json file
 vol_sj.commit_provenance()  # generates file://.../provenance json file
 
 # split ssv_ids into multiple slices for processing
-# chunksize = len(ssv_ids) // n_proc
 chunksize = len(ssv_ids) // n_proc
+# chunksize = (len(ssv_ids)/2) // n_proc
 proc_slices = []
 
 for i_proc in range(n_proc):
     chunkstart = int(i_proc * chunksize)
     # make sure to include the division remainder for the last process
-    chunkend = int((i_proc + 1) * chunksize) if i_proc < n_proc - 1 else len(ssv_ids)
+    chunkend = int((i_proc + 1) * chunksize) if i_proc < n_proc - 1 else (len(ssv_ids)/2)
     proc_slices.append(np.s_[chunkstart:chunkend])
 
 print(f"proc_slices: {proc_slices}")
@@ -99,13 +100,16 @@ cache = CacheService(
     config=config,
     compress=True,
 )
-mesh_meta = PrecomputedMetadata(
-    cloudpath, config, cache, volume_info
-)
-mesh_source_sv = PrecomputedMeshSource(mesh_meta, cache, config)
-mesh_source_vc = PrecomputedMeshSource(mesh_meta, cache, config)
-mesh_source_mi = PrecomputedMeshSource(mesh_meta, cache, config)
-mesh_source_sj = PrecomputedMeshSource(mesh_meta, cache, config)
+# mesh_meta_sv = PrecomputedMetadata(cloudpath+'sv/', config, cache, volume_info)
+mesh_meta_vc = PrecomputedMetadata(cloudpath+'vc/', config, cache, volume_info)
+mesh_meta_mi = PrecomputedMetadata(cloudpath+'mi/', config, cache, volume_info)
+mesh_meta_sj = PrecomputedMetadata(cloudpath+'sj/', config, cache, volume_info)
+
+
+# mesh_source_sv = PrecomputedMeshSource(mesh_meta_sv, cache, config)
+mesh_source_vc = PrecomputedMeshSource(mesh_meta_vc, cache, config)
+mesh_source_mi = PrecomputedMeshSource(mesh_meta_mi, cache, config)
+mesh_source_sj = PrecomputedMeshSource(mesh_meta_sj, cache, config)
 
 sources = [mesh_source_sv, mesh_source_vc, mesh_source_mi, mesh_source_sj]
 
@@ -116,35 +120,44 @@ def process(slice):
         ssv = ssd.get_super_segmentation_object(int(ssv_id))
 
         if i % 10 == 0:
-            print(f'Computing... {10*i} of slice')            
+            print(f'Computing... {i} of slice')            
 
         ssv.load_attr_dict()
 
         # load each obj_type
-        for j, obj_type in enumerate(['sv']): #, 'vc', 'mi','sj']): # 'syn_ssv'
-            mesh = ssv.load_mesh(obj_type)
+        for j, obj_type in enumerate(['vc', 'mi','sj']): # add SVs here if desired
 
-            indices = np.array(mesh[0], dtype=np.uint32).reshape(-1, 3)
-            vertices = np.array(mesh[1], dtype=np.float32).reshape(-1, 3)
-            # num_vert = vertices.shape[0]
+            if obj_type == 'vc':
+                so_list = ssv.vcs
+            elif obj_type == 'mi':
+                so_list = ssv.mis
+            elif obj_type == 'sj':
+                so_list = ssv.sjs
 
-            curr_mesh = Mesh(
-                vertices=vertices, faces=indices, 
-                segid=ssv_id,
-                normals=None,
-                encoding_type='precomputed', 
-            )
-            curr_mesh.segid=ssv_id
-            print(f'Mesh: {curr_mesh}')
-            
-            sources[j].put(curr_mesh)
+            for so in so_list:    
+                mesh = helper.load_mesh(so)
+
+                indices = np.array(mesh[0], dtype=np.uint32).reshape(-1, 3)
+                vertices = np.array(mesh[1], dtype=np.float32).reshape(-1, 3)
+                # num_vert = vertices.shape[0]
+
+                curr_mesh = Mesh(
+                    vertices=vertices, faces=indices, 
+                    segid=so.id,
+                    normals=None,
+                    encoding_type='draco', 
+                )
+                curr_mesh.segid=so.id
+                print(f'Mesh: {curr_mesh}')
+                
+                sources[j+1].put(curr_mesh)
             
 
 with ProcessPoolExecutor(max_workers=n_proc) as executor:
     executor.map(process, proc_slices)
 
 
-vol_sv.mesh = mesh_source_sv
+# vol_sv.mesh = mesh_source_sv
 vol_vc.mesh = mesh_source_vc
 vol_mi.mesh = mesh_source_mi
 vol_sj.mesh = mesh_source_sj
