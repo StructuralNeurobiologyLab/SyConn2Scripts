cce2879fec4a24d0b185ef7f352474906cfa2b0c
arother <rother@neuro.mpg.de>
2022-11-15 14:53:19 +0100

    add cellorganelle_trans_func

--- scripts/syconn_processing/run_syn_generation_script.py
+++ scripts/syconn_processing/run_syn_generation_script.py
@@ -5,8 +5,11 @@
 from syconn import global_params
 from syconn.exec.exec_syns import  run_syn_generation
 
-#set trans_func_sj
+#taken from start_j0251_organelles with labels from current wd
+cellorganelle_transf_funcs = dict(sj=lambda x: (x == 1).astype('u1'),
+                                      vc=lambda x: (x == 2).astype('u1'),
+                                      mi=lambda x: (x == 3).astype('u1'))
 
 global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811"
 
-run_syn_generation(transf_func_sj_seg=)
+run_syn_generation(transf_func_sj_seg=cellorganelle_transf_funcs['sj'])
