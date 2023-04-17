a5c67af3cab807a990ae21c581722814450d7958
arother <rother@neuro.mpg.de>
2023-04-18 00:32:43 +0200

    update run_syn_generation for new rfc_syn_classifier

--- scripts/syconn_processing/run_syn_generation_script.py
+++ scripts/syconn_processing/run_syn_generation_script.py
@@ -14,9 +14,9 @@ global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_
 
 f_name = f'{global_params.wd}'
 
-log = initialize_logging('230416 generating syn_ssv sd, props', log_dir=f_name + '/logs/')
+log = initialize_logging('230417 generating syn_ssv sd, props', log_dir=f_name + '/logs/')
 log.info(f'start generation with cell organell transfer func {cellorganelle_transf_funcs}, syn and syn_ssv generating done starting 14th April 23. \n'
-         f'Start ')
+         f'Updated rfc_classifier with current syn_ssv and extended GT')
 
 run_syn_generation(transf_func_sj_seg=cellorganelle_transf_funcs['sj'], exclude_nodes=['cajalg002', 'cajalg003', 'cajalg004', 'cajalg005', 'cajalg006', 'cajalg007'], overwrite=False)
 
