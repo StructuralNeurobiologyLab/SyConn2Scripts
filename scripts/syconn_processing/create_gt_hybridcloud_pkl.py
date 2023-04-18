99335cddeb41aebfcc4a0ac414e77cbc7b988e79
arother <rother@neuro.mpg.de>
2023-04-18 14:17:21 +0200

    update gt_hybridcloud script for cahcing hybrid clouds for training; add logging

--- scripts/syconn_processing/create_gt_hybridcloud_pkl.py
+++ scripts/syconn_processing/create_gt_hybridcloud_pkl.py
@@ -1,34 +1,7 @@
 #generate hybrid cloud pkl for cnn_training to increase speed
 
-from syconn.handler.prediction_pts import _load_ssv_hc
-from syconn.reps.super_segmentation import SuperSegmentationDataset
-from syconn.handler.basics import write_obj2pkl, load_pkl2obj
-from syconn import global_params
-import pandas as pd
-import numpy as np
-from functools import partial
-from multiprocessing import pool
-from tqdm import tqdm
-from scipy.spatial import cKDTree
-
-
-v6_gt = pd.read_csv("cajal/nvmescratch/projects/songbird/j0251/groundtruth/celltypes/j0251_celltype_gt_v6_j0251_72_seg_20210127_agglo2_IDs.csv", names = ["cellids", "celltype"])
-cellids = np.array(v6_gt["cellids"])
-filename = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811/celltype_training/hybrid_clouds_gt/"
-
-global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811"
-ssd = SuperSegmentationDataset(working_dir=global_params.config.working_dir)
-
-ssd_kwargs = dict(working_dir=global_params.wd, version='ctgt_v4')
-
-#parameters from default parameters of script cnn_celltype_ptcnv_j0251.py
-use_myelin = False
-use_syntype = True
-cellshape_only = False
-pts_feat_dict = dict(sv=0, mi=1, syn_ssv=3, syn_ssv_sym=3, syn_ssv_asym=4, vc=2, sv_myelin=5)
-
 #for this function use parts of syconn.handler.prediction_pts.pts_loader_scalar
-def get_hybrid_clouds(cellid, use_myelin, use_syntype, cellshape_only, filename):
+def get_hybrid_clouds(cellid, use_myelin, use_syntype, cellshape_only, filename, ssd, pts_feat_dict):
     ssv = ssd.get_super_segmentation_object(cellid)
     feat_dc = dict(pts_feat_dict)
     if cellshape_only:
@@ -60,11 +33,50 @@ def get_cKDtrees_pkl(cellid):
     ckdtree = cKDTree(hc.nodes)
     write_obj2pkl("%s/%i_kdtree.pkl" % (filename, cellid), ckdtree)
 
-p = pool.Pool()
-p.map(partial(get_hybrid_clouds, use_myelin = use_myelin, use_syntype = use_syntype,
-                     cellshape_only = cellshape_only, filename = filename), tqdm(cellids))
+if __name__ == '__main__':
 
+    from syconn.handler.prediction_pts import _load_ssv_hc
+    from syconn.reps.super_segmentation import SuperSegmentationDataset
+    from syconn.handler.basics import write_obj2pkl, load_pkl2obj
+    from syconn.handler.config import initialize_logging
+    from syconn import global_params
+    import pandas as pd
+    import numpy as np
+    from functools import partial
+    from multiprocessing import pool
+    from tqdm import tqdm
+    from scipy.spatial import cKDTree
 
-#p.map(get_cKDtrees_pkl, tqdm(cellids))
+    global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811"
+    log = initialize_logging('230418 generating training hybrid clouds', log_dir=global_params.wd + '/logs/')
+
+    v6_gt = pd.read_csv("cajal/nvmescratch/projects/songbird/j0251/groundtruth/celltypes/j0251_celltype_gt_v6_j0251_72_seg_20210127_agglo2_IDs.csv", names = ["cellids", "celltype"])
+    cellids = np.array(v6_gt["cellids"])
+    filename = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811/celltype_training/hybrid_clouds_gt/"
+
+    log.info(f'Create hybrid clouds used for training, will use gt from {v6_gt} \n'
+             f'Save hybrid clouds at {filename}')
+
+    ssd = SuperSegmentationDataset(working_dir=global_params.config.working_dir)
+
+    ssd_kwargs = dict(working_dir=global_params.wd, version='ctgt_v4')
+
+    #parameters from default parameters of script cnn_celltype_ptcnv_j0251.py
+    use_myelin = False
+    use_syntype = True
+    cellshape_only = False
+    pts_feat_dict = dict(sv=0, mi=1, syn_ssv=3, syn_ssv_sym=3, syn_ssv_asym=4, vc=2, sv_myelin=5)
+
+    log.info(f'Parameters set to use_myelin = {use_myelin}, use_syntype = {use_syntype}, cellshape_only = {cellshape_only}')
+    log.info(f'uses feature dict: {pts_feat_dict}')
+
+    log.info(f'Will generate {len(cellids)} now')
+    p = pool.Pool()
+    p.map(partial(get_hybrid_clouds, use_myelin = use_myelin, use_syntype = use_syntype,
+                         cellshape_only = cellshape_only, filename = filename, ssd = ssd, pts_feat_dict = pts_feat_dict), tqdm(cellids))
+
+    #p.map(get_cKDtrees_pkl, tqdm(cellids))
+
+    log.info('Generating hybrid clouds for celltype training done')
 
 
--- scripts/syconn_processing/run_syn_generation_script.py
+++ scripts/syconn_processing/run_syn_generation_script.py
@@ -1,25 +1,26 @@
 #call exec_syn run_syn_generation
 #generates syn_ssv dataset but here with cs_dataset already existing
 
-from syconn.handler.config import initialize_logging
-from syconn import global_params
-from syconn.exec.exec_syns import  run_syn_generation
+if __name__ == '__main__':
+    from syconn.handler.config import initialize_logging
+    from syconn import global_params
+    from syconn.exec.exec_syns import  run_syn_generation
 
-#taken from start_j0251_organelles with labels from current wd
-cellorganelle_transf_funcs = dict(sj=lambda x: (x == 1).astype('u1'),
-                                      vc=lambda x: (x == 2).astype('u1'),
-                                      mi=lambda x: (x == 3).astype('u1'))
+    #taken from start_j0251_organelles with labels from current wd
+    cellorganelle_transf_funcs = dict(sj=lambda x: (x == 1).astype('u1'),
+                                          vc=lambda x: (x == 2).astype('u1'),
+                                          mi=lambda x: (x == 3).astype('u1'))
 
-global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811"
+    global_params.wd = "cajal/nvmescratch/projects/data/songbird_tmp/j0251/j0251_72_seg_20210127_agglo2_syn_20220811"
 
-f_name = f'{global_params.wd}'
+    f_name = f'{global_params.wd}'
 
-log = initialize_logging('230417 generating syn_ssv sd, props', log_dir=f_name + '/logs/')
-log.info(f'start generation with cell organell transfer func {cellorganelle_transf_funcs}, syn and syn_ssv generating done starting 14th April 23. \n'
-         f'Updated rfc_classifier with current syn_ssv and extended GT')
+    log = initialize_logging('230417 generating syn_ssv sd, props', log_dir=f_name + '/logs/')
+    log.info(f'start generation with cell organell transfer func {cellorganelle_transf_funcs}, syn and syn_ssv generating done starting 14th April 23. \n'
+             f'Updated rfc_classifier with current syn_ssv and extended GT')
 
-run_syn_generation(transf_func_sj_seg=cellorganelle_transf_funcs['sj'], exclude_nodes=['cajalg002', 'cajalg003', 'cajalg004', 'cajalg005', 'cajalg006', 'cajalg007'], overwrite=False)
+    run_syn_generation(transf_func_sj_seg=cellorganelle_transf_funcs['sj'], exclude_nodes=['cajalg002', 'cajalg003', 'cajalg004', 'cajalg005', 'cajalg006', 'cajalg007'], overwrite=False)
 
-log.info('syn_ssv generation finished')
+    log.info('syn_ssv generation finished')
 
 
